aad0040a2cb6951172c483ff215715ea04acb709
Benjalaazshah <>
2022-03-06 00:03:03 -0600

    Added the _log method to the Terminal class.

--- sources/index.ts
+++ sources/index.ts
@@ -1,6 +1,4 @@
-export type TerminalConstructorData = {
-	[Key in keyof TerminalData]?: TerminalData[Key];
-};
+import C from "kleur";
 
 export interface TerminalData {
 	showLevelName: boolean;
@@ -11,9 +9,60 @@ export interface TerminalData {
 
 export class Terminal {
 	readonly data: TerminalData;
-	readonly startTime: number;
+	readonly startTime: Date;
+	timeInLastLog: Date;
 
-	constructor(data?: TerminalConstructorData) {
+	private _log(level: "ERROR" | "LOG", message: string) {
+		const time = new Date();
+		let output = "";
+		const levelColor = level === "ERROR" ? (x: string) => C.underline().dim().red(x) : (x: string) => C.bold().black(x);
+
+		function formatChangeInTime(from: Date, prefix: string) {
+			let formattedChangeInTime = "";
+			let remainder = time.getTime() - from.getTime();
+
+			function addUnitOfTime(unitValueInMilliseconds: number, unitName: string) {
+				const unitCount = Math.floor(remainder / unitValueInMilliseconds);
+
+				remainder = remainder % unitValueInMilliseconds;
+				formattedChangeInTime += unitCount !== 0 ? levelColor(prefix + unitCount + unitName) + " " : "";
+			}
+
+			addUnitOfTime(31536000000, "y"); // YEARS
+			addUnitOfTime(2592000000, "m"); // MONTHS
+			addUnitOfTime(86400000, "d"); // DAYS
+			addUnitOfTime(3600000, "h"); // HOURS
+			addUnitOfTime(60000, "min"); // MINUTES
+			addUnitOfTime(1000, "s"); // SECONDS
+			addUnitOfTime(1, "ms"); // MILLISECONDS
+
+			return formattedChangeInTime;
+		}
+
+		// Should look like: [ ERROR ]
+		if (this.data.showLevelName) output += `[ ${levelColor(level)} ]\t`;
+
+		// Should look like: [ 12d/5m/2011y | 13:43:10.23 ]
+		if (this.data.showTimestamp) {
+			output += `[ ${levelColor(`${time.getDate()}d/${time.getMonth()}m/${time.getFullYear()}y`)} |`;
+			output += ` ${levelColor(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}.${time.getMilliseconds()}`)} ]\t`;
+		}
+
+		// Should look like: [ 5y 1m 15h 51min 7s 300ms | +31min +5s +903ms ]
+		if (this.data.showRelativeTimestamp || this.data.showTimestampRelativeToLastLog) {
+			output += "[ ";
+			if (this.data.showRelativeTimestamp) output += formatChangeInTime(this.startTime, "");
+			if (this.data.showRelativeTimestamp && this.data.showTimestampRelativeToLastLog) output += "| ";
+			if (this.data.showTimestampRelativeToLastLog) output += formatChangeInTime(this.timeInLastLog, "+");
+			output += "]\t";
+		}
+
+		output += `${message}\n`;
+		process.stdout.write(output);
+		this.timeInLastLog = time;
+	}
+
+	constructor(data?: Partial<TerminalData>) {
 		const defaultData = {
 			showLevelName: false,
 			showRelativeTimestamp: true,
@@ -21,7 +70,8 @@ export class Terminal {
 			showTimestampRelativeToLastLog: true
 		};
 
-		this.startTime = Date.now();
+		this.startTime = new Date();
+		this.timeInLastLog = this.startTime;
 		this.data = data ? Object.assign({}, defaultData, data) : defaultData;
 	}
 }
