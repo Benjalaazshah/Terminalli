923623a27031b53da449c30c587fe736f6a532a8
Benjalaazshah <>
2022-04-05 18:53:39 -0500

    Added extra JSDoc comments.

--- sources/index.ts
+++ sources/index.ts
@@ -1,13 +1,42 @@
 import C from "kleur";
 
 /**
- * Data for the `Terminal` class.
+ * Customization options for the `Terminal` class.
  */
 export interface TerminalData {
+	/**
+	 * If true, each message logged to the terminal will have a date corresponding to when the message was logged attached to it.
+	 *
+	 * `[ 12d/5m/2011y ] foo`
+	 */
 	showDate: boolean;
+
+	/**
+	 * If true, each message logged to the terminal will have the name of the level of the message attached to it.
+	 *
+	 * `[ ERROR ] WHAT WILL I DO?!`
+	 */
 	showLevelName: boolean;
+
+	/**
+	 * If true, each message logged to the terminal will have a timestamp relative to the creation of this particular instance of the `Terminal` class to which this message has been logged.
+	 *
+	 * `[ 5y 1m 15h 51min 7s 300ms ] A long time has passed.`
+	 */
 	showRelativeTimestamp: boolean;
+
+	/**
+	 * If true, each message logged to the terminal will have a timestamp corresponding to the exact time the message was logged.
+	 *
+	 * `[ 13:43:10.23 ] bar`
+	 */
 	showTimestamp: boolean;
+
+	/**
+	 * If true, each message logged to the terminal will have a timestamp relative to when the previous message was logged to the terminal.
+
+	 * `[ +31min +5s +903ms ] It took forever!`
+	 */
 	showTimestampRelativeToLastLog: boolean;
 }
 
