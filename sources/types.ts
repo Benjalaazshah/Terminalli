32ef650c75794ed6ea146c340d35213d7261d160
Benjalaazshah <Benjalaazshah@gmail.com>
2022-04-23 21:31:50 -0500

    Added some JSDoc comments.

--- sources/types.ts
+++ sources/types.ts
@@ -1,11 +1,32 @@
 import { Kleur } from "kleur";
 
+/**
+ * Represents an ANSI color.
+ */
 export type Color = keyof Kleur;
+
+/**
+ * Same as `TerminalConstructorData<string>`, but all properties are required and read-only. This interface is used for the `data` property in the `Terminal` class.
+ */
 export type TerminalData = Readonly<Required<TerminalConstructorData<string>>>;
 
+/**
+ * Represents a category to which you can log. Levels are usually used as categories of importance.
+ */
 export interface Level<L extends string> {
+	/***
+	 * Any ANSI colors/formats which you want any messages logged to this level to have their timestamps highlighted in.
+	 */
 	color: Color[];
+
+	/**
+	 * Whether or not should each message logged to this level be sent to `process.stderr` (standard error stream) instead of `process.stdout` (standard out stream).
+	 */
 	isError: boolean;
+
+	/**
+	 * The name of this level.
+	 */
 	name: L;
 }
 
@@ -13,8 +34,29 @@ export interface Level<L extends string> {
  * Customization options for the `Terminal` class.
  */
 export interface TerminalConstructorData<L extends string> {
+	/**
+	 * Whether or not to capitalize the name of a log's corresponding level when it's attached to the log. Keep in mind that you can only see the name of a log's corresponding level when `showLevelName` is also true.
+	 *
+	 * **No capitalization:**
+	 *
+	 * `[ info ] Something very interesting happened.`
+	 *
+	 * **Capitalization:**
+	 *
+	 * `[ INFO ] Something very interesting happened.`
+	 */
 	capitalizeLevelName?: boolean;
+
+	/**
+	 * A list of categories which you can log to.
+	 */
 	levels: Level<L>[];
+
+	/**
+	 * Whether or not to show a cool arrow before a log's message.
+	 *
+	 * `>> baz`
+	 */
 	showArrow?: boolean;
 
 	/**
@@ -27,7 +69,7 @@ export interface TerminalConstructorData<L extends string> {
 	/**
 	 * If true, each message logged to the terminal will have the name of the level of the message attached to it.
 	 *
-	 * `[ ERROR ] WHAT WILL I DO?!`
+	 * `[ fatal ] WHAT WILL I DO?!`
 	 */
 	showLevelName?: boolean;
 
@@ -60,6 +102,7 @@ export interface TerminalConstructorData<L extends string> {
 
 	/**
 	 * If true, each message logged to the terminal will have a timestamp relative to when the previous message was logged to the terminal.
+	 *
 	 * `[ +31min +5s +903ms ] It took forever!`
 	 */
 	showTimestampRelativeToLastLog?: boolean;
